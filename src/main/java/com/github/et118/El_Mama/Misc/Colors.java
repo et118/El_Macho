50e8235934a5498df919b60fc5f114bda3638157
et118 <51513175+et118@users.noreply.github.com>
2022-01-30 00:11:53 +0100

    added colors class

---
+++ src/main/java/com/github/et118/El_Mama/Misc/Colors.java
@@ -0,0 +1,9 @@
+package com.github.et118.El_Mama.Misc;
+
+import discord4j.rest.util.Color;
+
+public class Colors {
+    public static Color SUCCESS = Color.of(0x4fff55);
+    public static Color ERROR = Color.of(0x702607);
+    public static Color INFO = Color.of(0x000000);
+}
